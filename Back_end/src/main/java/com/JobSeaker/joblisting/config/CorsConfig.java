fe2150b13f0e19a26f4bde2df753a101815c4eca
Prince Rajput <pr7509645@gmail.com>
2025-11-22 23:41:05 +0530

    Update CorsConfig.java
    Add config 
    file

--- Back_end/src/main/java/com/JobSeaker/joblisting/config/CorsConfig.java
+++ Back_end/src/main/java/com/JobSeaker/joblisting/config/CorsConfig.java
@@ -1,24 +1,23 @@
-//package com.JobSeaker.joblisting.config;
-//
-//import org.springframework.context.annotation.Bean;
-//package com.example.joblisting.config;
-//
-//
-//import org.springframework.context.annotation.Configuration;
-//import org.springframework.web.servlet.config.annotation.CorsRegistry;
-//import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
-//@Configuration
-//public class CorsConfig {
-//    @Bean
-//    public WebMvcConfigurer corsConfigurer() {
-//        return new WebMvcConfigurer() {
-//            @Override
-//            public void addCorsMappings(CorsRegistry registry) {
-//                registry.addMapping("/**")
-//                        .allowedOrigins("http://localhost:3001") // your React app URL
-//                        .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
-//                        .allowedHeaders("*");
-//            }
-//        };
-//    }
-//}
+package com.JobSeaker.joblisting.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class CorsConfig {
+
+    @Bean
+    public WebMvcConfigurer corsConfigurer() {
+        return new WebMvcConfigurer() {
+            @Override
+            public void addCorsMappings(CorsRegistry registry) {
+                registry.addMapping("/**")
+                        .allowedOrigins("*")
+                        .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
+                        .allowedHeaders("*");
+            }
+        };
+    }
+}
